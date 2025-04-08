b432ff0ef5c53f771e0335e98359c828a8ff8e16
Tehmina <mina.sadiqali@gmail.com>
2025-04-07 21:47:27 -0400

    Added House Class and Constructors

--- ide2/src/House.java
+++ ide2/src/House.java
@@ -1,3 +1,16 @@
 public class House {
-
+    private String address;
+    private double price;
+    private double downPayment;
+    private double mortgageRate;
 }
+
+// Constructor to initialize Management system with address, price, downPayment, and mortgageRate
+public House(String address, double price, double downPayment, double mortgageRate) {
+    this.address = address;
+    this.price = price;
+    this.downPayment = downPayment;
+    this.mortgageRate = mortgageRate;
+}
+
+
---
+++ ide2/src/MortgageManagement.java
@@ -0,0 +1,8 @@
+/*
+Tehmina Sad
+IDE Assignment #2 - Real Estate Listings Management System
+CSC 222
+Tehmina Sadiq Ali
+ */
+
+}
